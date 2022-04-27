b52cdbd0f66b52c9cef991ab67fc67665c352f0d
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-04-26 21:51:44 -0300

    Feat: Adding style.

--- password-generator/src/App.js
+++ password-generator/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="password-container">
+        <div className="password-header">
+          <div className="password">
+            <button>Copy to clipboard</button>
+          </div>
+        </div>
+        <div className="password-body">
+          <div className="form-control">
+            <label htmlFor="len">
+              Password Length
+            </label>
+            <input type="number" id="len" min="4" max="40"/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="lower">
+            Contain Lowercase Letters
+            </label>
+            <input type="checkbox" id="lower"/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="number">
+            Contain Numbers
+            </label>
+            <input type="checkbox" id="number"/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="symbol">
+            Contain Symbols
+            </label>
+            <input type="checkbox" id="symbol"/>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
